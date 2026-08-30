597263ec63cbf92c6776efc208a1b795687dd474
irinakurnishova <kurnishovaiv@gmail.com>
2026-08-30 17:52:00 +0200

    generate HTML for the cart items

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import './HomePage.css';
 import {useEffect, useState} from "react";
 import axios from 'axios';
 import {Header} from "../components/Header.jsx";
+import {formatMoney} from "../utils/money.js";
 import Checkmark from "../assets/images/icons/checkmark.png"
 
 export function HomePage ({cart}) {
@@ -43,7 +44,7 @@ export function HomePage ({cart}) {
                                 </div>
 
                                 <div className="product-price">
-                                    ${(product.priceCents / 100).toFixed(2)}
+                                    {formatMoney(product.priceCents)}
                                 </div>
 
                                 <div className="product-quantity-container">
