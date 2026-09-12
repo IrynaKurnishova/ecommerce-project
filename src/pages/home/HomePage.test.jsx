8f8d0b1ee69820e98c480638acaf13c89bd1698c
irinakurnishova <kurnishovaiv@gmail.com>
2026-09-12 15:56:22 +0200

    Create integration test for Home Page

--- src/pages/home/HomePage.test.jsx
+++ src/pages/home/HomePage.test.jsx
@@ -1,7 +1,6 @@
 import {it, expect, describe, vi, beforeEach} from 'vitest';
 import {render, screen, within} from "@testing-library/react";
 import {MemoryRouter} from "react-router";
-import Product from "./Product.jsx";
 import userEvent from "@testing-library/user-event";
 import axios from "axios";
 import {HomePage} from "./HomePage.jsx";
@@ -63,5 +62,43 @@ describe('HomePage component', () => {
         expect(
             within(productContainers[1]).getByText('Intermediate Size Basketball')
         ).toBeInTheDocument();
+    })
+
+    it('adds products to the cart when clicking Add to Cart buttons', async () => {
+        const user = userEvent.setup();
+
+        render(
+            <MemoryRouter>
+                <HomePage cart={[]} loadCart={loadCart}/>
+            </MemoryRouter>
+        );
+
+        const productContainers = await screen.findAllByTestId('product-container');
+
+        const firstAddToCartButton = within(productContainers[0]).getByTestId('add-to-cart-button');
+        await user.click(firstAddToCartButton);
+
+        const secondAddToCartButton = within(productContainers[1]).getByTestId('add-to-cart-button');
+        await user.click(secondAddToCartButton);
+
+        expect(axios.post).toHaveBeenNthCalledWith(
+            1,
+            '/api/cart-items',
+            {
+                productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                quantity: 1
+            }
+        );
+
+        expect(axios.post).toHaveBeenNthCalledWith(
+            2,
+            '/api/cart-items',
+            {
+                productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+                quantity: 1
+            }
+        );
+
+        expect(loadCart).toHaveBeenCalledTimes(2);
     })
 })
